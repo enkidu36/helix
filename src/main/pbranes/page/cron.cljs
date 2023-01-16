8b98c3d24b2790201d3f4b0752ebdbf625ad301d
Joe Savoie <enkidu36@yahoo.com>
2023-01-16 14:01:24 -0600

    init code to call to pbranes-api server

--- src/main/pbranes/page/cron.cljs
+++ src/main/pbranes/page/cron.cljs
@@ -1,7 +1,24 @@
 (ns pbranes.page.cron
-  (:require [helix.core :refer [$ <> defnc]]
-            [helix.dom :as d]))
+  (:refer-clojure :exclude [parse-double])
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require  [helix.core :refer [$ <> defnc]]
+             [cljs.core.async :refer [<!]]
+             [cljs-http.client :as http]
+             [helix.hooks :as hooks]
+             [helix.dom :as d]))
 
+(defn make-remote-call [endpoint fn-state]
+  (go (let [response (<! (http/get endpoint {:with-credentials? false}))
+            data (:body response)]
+        (fn-state data))))
 
 (defnc cron-page []
-  (<> "hello"))
+  (let [[state set-state] (hooks/use-state {:value "init state"})]
+    
+    (hooks/use-effect
+     []
+     :once
+     (make-remote-call "http://localhost:3000/testing" (fn [data] (set-state assoc :value data))))
+
+    (d/div (:value state))))
+
